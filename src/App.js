cbb30d5b5a5fc39de6ba7d55b554a2c82d925f0e
Dave <docbonson@gmail.com>
2023-11-23 16:58:35 -0600

    updated about page and styles

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import About from './about/About'
 import Skills from './skills/Skills'
 import Projects from './projects/Projects'
 import Contact from './contact/Contact'
+import Background from './background/Background.js'
+import PlayerStats from './playerStats/PlayerStats.js'
 import './styles/app.css'
 
 const App = () => {
   return (
     <Router>
       <Nav />
+      <Background />
       <Routes>
-        <Route path="/" element={About} />
-        <Route path="/skills" element={Skills} />
-        <Route path="/projects" element={Projects} />
-        <Route path="/contact" element={Contact} />
+        <Route path="/" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <PlayerStats />
     </Router>
   )
 }
---
+++ src/about/subheadingsData.js
@@ -0,0 +1,49 @@
+const subheadingsData = {
+  1: [
+    {
+      title: 'About Me',
+      content: (
+        <>
+          <p>
+            Dedicated front-end developer with over five years of hands-on
+            experience in React and JavaScript. Transitioning from a vibrant
+            career in music and talent management, I bring a unique blend of
+            creativity and technical prowess to software development. Focused on
+            good design principles and meticulous attention to detail, I have
+            successfully rebuilt websites and streamlined operational processes.
+            Proficient in modern libraries like Tailwind and Typescript, I
+            specialize in crafting intuitive and visually appealing user
+            interfaces, ensuring exceptional user experiences.
+            <br />
+            <br />
+            In my spare time I like to read and write. I love Kurt Vonnegut,
+            Stephen King, and Octavia Butler. I'm also a huge weather nerd and
+            Twister is my favorite movie.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: '2021 Associates Degree',
+      content: (
+        <>
+          <p>Madison College - Madison, WI</p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: '2021 - 2024 Software Engineer II',
+      content: (
+        <>
+          <p>TDS Telecom</p>
+        </>
+      ),
+    },
+  ],
+}
+
+export default subheadingsData
