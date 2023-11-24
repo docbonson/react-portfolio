32856fe7a679194fa0a062b4a99360f68fa3616a
Dave <docbonson@gmail.com>
2023-11-23 19:30:56 -0600

    added skills page and styling

--- src/skills/SkillsMenu.js
+++ src/skills/SkillsMenu.js
@@ -1,8 +1,61 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
 import '../styles/skillsMenu.css'
+import skills from './skillsData'
+import frontendIcon from '../assets/eagle-emblem.png'
+import backendIcon from '../assets/hawk-emblem.png'
 
-export default class SkillsMenu extends Component {
-  render() {
-    return <div>SkillsMenu</div>
+const SkillsMenu = () => {
+  const [activeMenuItem, setActiveMenuItem] = useState(1)
+
+  const handleMenuItemClick = (menuItem) => {
+    setActiveMenuItem(menuItem)
   }
+
+  const renderContent = (skills) => {
+    if (!skills) {
+      return null
+    }
+
+    return skills.map((skill, index) => (
+      <div key={index} className={`skill-sub-container-${activeMenuItem}`}>
+        <h3>{skill.title}</h3>
+        <div className="level-container">
+          {[...Array(6)].map((_, i) => (
+            <div
+              key={i}
+              className={`level-point ${
+                i < skill.level ? 'filled' : 'unfilled'
+              }`}
+            />
+          ))}
+        </div>
+      </div>
+    ))
+  }
+
+  const menuItems = ['FRONT-END', 'BACK-END']
+  const currentIcon = activeMenuItem === 1 ? frontendIcon : backendIcon
+
+  return (
+    <div className="skill-menu">
+      {menuItems.map((item, index) => (
+        <div
+          key={index}
+          className={classNames('skill-item', {
+            activeSkill: activeMenuItem === index + 1,
+          })}
+          onClick={() => handleMenuItemClick(index + 1)}
+        >
+          <h2 className="skill-title">{item}</h2>
+        </div>
+      ))}
+      <img className="skill-icon" src={currentIcon} alt="current skill" />
+      <div className="skill-sub-container">
+        {renderContent(skills[activeMenuItem])}
+      </div>
+    </div>
+  )
 }
+
+export default SkillsMenu
---
+++ src/skills/skillsData.js
@@ -0,0 +1,51 @@
+const skillsData = {
+  1: [
+    {
+      title: 'HTML',
+      level: 5,
+    },
+    {
+      title: 'CSS',
+      level: 5,
+    },
+    {
+      title: 'React JS',
+      level: 5,
+    },
+    {
+      title: 'JavaScript',
+      level: 4,
+    },
+    {
+      title: 'TypeScript',
+      level: 3,
+    },
+    {
+      title: 'Tailwind CSS',
+      level: 5,
+    },
+    {
+      title: 'Figma',
+      level: 3,
+    },
+    {
+      title: 'Agile',
+      level: 5,
+    },
+  ],
+  2: [
+    {
+      title: 'Python',
+      level: 2,
+    },
+    {
+      title: 'Git',
+      level: 5,
+    },
+    {
+      title: 'Node.js',
+      level: 2,
+    },
+  ],
+}
+export default skillsData
